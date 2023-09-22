6efd9b735bf06d2e6138ae578cd4ce3875282045
Thea <TheaVanherst@gmail.com>
2023-09-22 10:54:10 +0100

    Bug fixes & Social reorder.

--- frontEnd/src/lib/controllers/navigationDirectories.js
+++ frontEnd/src/lib/controllers/navigationDirectories.js
@@ -4,14 +4,14 @@ const socialMedias =
     [
         {   title: "X",         name: "X / Twitter",    imagePath:"twitterLogo",        path: "twitter.com/TheaVanherst"},
         {   title: "FA",        name: "FurAffinity",    imagePath:"furaffinityLogo",    path: "furaffinity.net/user/theavanherst"},
+        {   title: "bSky",      name: "BlueSky",        imagePath:"blueskyLogo",        path: "bsky.app/profile/vanh.art"},
         {   title: "Git",       name: "Github",         imagePath:"githubLogo",         path: "github.com/TheaVanherst"},
-        {   title: "Store",     name: "Redbubble",      imagePath:"redbubbleLogo",      path: "redbubble.com/people/Vanh-art"},
-        {   title: "Kofi",      name: "Donate",         imagePath:"kofiLogo",           path: "ko-fi.com/theavanherst"},
-        {   title: "Paypal",    name: "Paypal",         imagePath:"paypalLogo",         path: "paypal.me/TheaVanherst"},
-        {   title: "Blue",      name: "BlueSky",        imagePath:"blueskyLogo",        path: "bsky.app/profile/vanh.art"},
         {   title: "Steam",     name: "Workshop",       imagePath:"steamLogo",          path: "steamcommunity.com/id/VoreMommy/myworkshopfiles/"},
-        {   title: "YT",        name: "Youtube",        imagePath:"youTubeLogo",        path: "www.youtube.com/channel/UCqbZe2XMkd98nXu_dUlObKA"},
-        {   title: "BC",        name: "Bandcamp",       imagePath:"bandcampLogo",       path: "bandcamp.com/theavanherst"},
+        {   title: "Kofi",      name: "Kofi",           imagePath:"kofiLogo",           path: "ko-fi.com/theavanherst"},
+        {   title: "Store",     name: "Redbubble",      imagePath:"redbubbleLogo",      path: "redbubble.com/people/Vanh-art"},
+        {   title: "Paypal",    name: "Paypal",         imagePath:"paypalLogo",         path: "paypal.me/TheaVanherst"},
+        // {   title: "BC",        name: "Bandcamp",       imagePath:"bandcampLogo",       path: "bandcamp.com/theavanherst"},
+        // {   title: "YT",        name: "Youtube",        imagePath:"youTubeLogo",        path: "www.youtube.com/channel/UCqbZe2XMkd98nXu_dUlObKA"},
     ];
 
 const redirector = " 𝚊𝚝"
--- frontEnd/src/routes/+layout.js
+++ frontEnd/src/routes/+layout.js
@@ -4,6 +4,6 @@ import { nsfw } from "$lib/controllers/accessibilityController.js";
 export const prerender = false;
 export const trailingSlash = 'always';
 
-export async function load({ctx, params }) {
+export async function load({ params }) {
     nsfw.set(params.sfw === 'nsfw');
 }
